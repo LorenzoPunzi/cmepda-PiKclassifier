501631fc18b070f0af29494659b768b5b8c4e3df
Lorenzo Punzi <punzilorenzo@gmail.com>
2023-01-17 18:15:07 +0100

    fixing commit 2

--- training.py
+++ training.py
@@ -7,30 +7,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from keras.layers import Dense, Input
 from keras.models import Model
+import time
 
-training_set = np.loadtxt('0.5_nparray.txt')
+np.random.seed(int(time.time()))
+
+training_set = np.loadtxt('0.4_nparray.txt')
 
 x = training_set[:,0]  # VARIABLE NAMES SHOULD BE CHANGED APPROPRIATELY 
 y = training_set[:,1]
 
 inputlayer=Input(shape=(1,))
-hiddenlayer1 = Dense(5, activation='relu')(inputlayer)
-hiddenlayer2 = Dense(10, activation='relu')(hiddenlayer1)
-hiddenlayer3 = Dense(10, activation='relu')(hiddenlayer2)
-hiddenlayer4 = Dense(10, activation='relu')(hiddenlayer3)
-outputlayer = Dense(1, activation='sigmoid')(hiddenlayer4)
+hiddenlayer = Dense(2, activation='relu')(inputlayer)
+hiddenlayer = Dense(5, activation='relu')(hiddenlayer)
+hiddenlayer = Dense(5, activation='relu')(hiddenlayer)
+outputlayer = Dense(1, activation='sigmoid')(hiddenlayer)
 deepnn = Model(inputs=inputlayer, outputs=outputlayer)
 deepnn.compile(loss='binary_crossentropy', optimizer='adam')
 deepnn.summary()
 
-history = deepnn.fit(x,y,validation_split=0.5,epochs=500,verbose=1)
+history = deepnn.fit(x,y,validation_split=0.4,epochs=100,verbose=1)
 
 plt.figure('Losses')
 plt.plot(history.history['val_loss'])
 plt.plot(history.history['loss'])
 
-test_set = np.loadtxt('0.4_nparray.txt')[:,0]
+test_set = np.loadtxt('0.5_nparray.txt')[:,0]
 pred_array = deepnn.predict(test_set)
-print(type(pred_array))
+print(test_set)
 f_pred = np.sum(pred_array)
 print(f'The predicted K fraction is : {f_pred/len(pred_array)}')
+print('Max prediction :',np.max(pred_array))
+print('Min prediction :',np.min(pred_array))
+print(pred_array)
+#plt.show()
