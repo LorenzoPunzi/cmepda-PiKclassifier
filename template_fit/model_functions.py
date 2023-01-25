40ea27cb053da9c6e3e86db095f2f77878da2851
rubenforti <rubenforti27@gmail.com>
2023-01-25 17:36:50 +0100

    Model functions improved and both MC fitted

--- template_fit/model_functions.py
+++ template_fit/model_functions.py
@@ -5,45 +5,105 @@ import ROOT
 import numpy as np
 
 
-def BreitWigner():
-    arg1 = '2./3.1416'
-    arg2 = '([1]*[1])*([2]*[2])'  # Gamma=par[1]  M=par[2]
-    arg3 = '((x*x) - ([2]*[2]))*((x*x) - ([2]*[2]))'
-    arg4 = '(x*x*x*x)*(([1]*[1])/([2]*[2]))'
-
-    str = '[0]*' + arg1 + '*' + arg2 + '/(' + arg3 + ' + ' + arg4 + ')'
-
-    return str
-
-
-def DoubleGaussian():
+def BreitWigner(LowLim, UpLim, funcname='BW', pars=(2000, 5.28, 0.5)):
     """
-    Returns a string that describes a function formed by a weighted sum of
-    two Gaussians. The parameters are written in C++ ROOT style and represent
-    the following quantities:
+    Returns a TF1 function corresponding to a relativistic Breit-Wigner.
+    The parameters are written in C++ ROOT style and represent the following
+    quantities:
+        [0] = Normalization
+        [1] = Mass
+        [2] = Gamma
+
+    Parameters
+    ----------
+    LowLim: double
+        Lower limit of the x axis
+    UpLim: double
+        Upper limit of the x axis
+    funcname: string
+        Name of the function created
+    pars: tuple of double
+        Initial parameters given to the function
+    """
+    arg_gamma = 'TMath::Sqrt([1]*[1]*([2]*[2] + [1]*[1]))'
+    arg_kappa = '([1]*[2]*' + arg_gamma + \
+        '/TMath::Sqrt([1]*[1] + ' + arg_gamma + '))'
+    arg_denom_1 = '(x*x - [1]*[1])*(x*x - [1]*[1])'
+    str = '[0]*6e-4*0.9*' + arg_kappa + \
+        '/(' + arg_denom_1 + '+ [1]*[1]*[2]*[2])'
+
+    function = ROOT.TF1(funcname, str, LowLim, UpLim, 3)
+    print(type(function))
+
+    [function.SetParameter(k, pars[k]) for k in range(3)]
+    function.SetParLimits(0, 0., 1e10)
+    function.SetParLimits(1, LowLim, UpLim)
+    function.SetParLimits(2, 0., UpLim-LowLim)
+
+    return function
+
+
+def DoubleGaussian(LowLim, UpLim, funcname='DoubleGaussian',
+                   pars=(1.4e5, 0.5, 5.28, 0.02, 5.28, 0.02)):
+    """
+    Returns a TF1 function corresponding to a weighted sum of two Gaussians.
+    The parameters are written in C++ ROOT style and represent the following
+    quantities:
         [0] = Normalization
         [1] = Fraction of first gaussian elements in distribution
         [2], [3] = Mean and Sigma for first Gaussian
         [4], [5] = Mean and Sigma for second Gaussian
+
+    Parameters
+    ----------
+    LowLim: double
+        Lower limit of the x axis
+    UpLim: double
+        Upper limit of the x axis
+    funcname: string
+        Name of the function created
+    pars: tuple of double
+        Initial parameters given to the function
     """
     const = '1/2.5066'
     gaus1 = 'TMath::Exp(-(x-[2])*(x-[2])/(2*[3]))/[3]'
     gaus2 = 'TMath::Exp(-(x-[4])*(x-[4])/(2*[5]))/[5]'
+    str = '[0]*6e-4*([1]*' + gaus1 + ' + (1-[1])*' + gaus2 + ')*' + const
 
-    str = '[0]*([1]*' + gaus1 + ' + (1-[1])*' + gaus2 + ')*' + const
+    function = ROOT.TF1(funcname, str, LowLim, UpLim, 6)
 
-    return str
+    [function.SetParameter(k, pars[k]) for k in range(6)]
+
+    function.SetParLimits(1, 0, 1)
+    function.SetParLimits(2, LowLim, UpLim)
+    function.SetParLimits(3, 0, UpLim-LowLim)
+    function.SetParLimits(4, LowLim, UpLim)
+    function.SetParLimits(5, 0, UpLim-LowLim)
+
+    return function
 
 
-def GaussJohnson():
+def GaussJohnson(LowLim, UpLim, funcname='Gauss+Johnson',
+                 pars=(1.4e05, 0.99, 1.75, 0.043, 5.29, 0.85, 5.19, 0.06)):
     """
-    Returns a string that describes a function formed by a weighted sum of
-    a Johnson and a Gaussian. The parameters are written in C++ ROOT style
+    Returns a TF1 function corresponding to a weighted sum of a Johnson and a
+    Gaussian. The parameters in the definition are written in C++ ROOT style
     and represent the following quantities:
         [0] = Normalization
         [1] = Fraction of first gaussian elements in distribution
         [2], [3], [4], [5] = Johnson parameters (need to be positive!)
         [6], [7] = Mean and Sigma for Gaussian
+
+    Parameters
+    ----------
+    LowLim: double
+        Lower limit of the x axis
+    UpLim: double
+        Upper limit of the x axis
+    funcname: string
+        Name of the function created
+    pars: tuple of double
+        Initial parameters given to the function
     """
     const = '1/2.5066'
     arg = '((x-[4])/[3])'
@@ -51,14 +111,20 @@ def GaussJohnson():
     expo = 'TMath::Exp(-0.5*' + expo_arg + '*' + expo_arg + ')'
     denom = 'TMath::Sqrt(1+(' + arg + '*' + arg + '))'
     johns = '([2]/[3])*' + expo + '/' + denom
-
     gaus = 'TMath::Exp(-(x-[6])*(x-[6])/(2*[7]))/[7]'
+    str = '[0]*6e-4*([1]*' + johns + ' + (1-[1])*' + gaus + ')*' + const
 
-    str = '[0]*([1]*' + johns + ' + (1-[1])*' + gaus + ')*' + const
+    function = ROOT.TF1(funcname, str, LowLim, UpLim, 8)
+    [function.SetParameter(k, pars[k]) for k in range(8)]
 
-    return str
+    function.SetParLimits(1, 0, 1)
+    [function.SetParLimits(k, 0., 10.) for k in range(2, 6)]
+    function.SetParLimits(6, LowLim, UpLim)
+    function.SetParLimits(7, 0, UpLim-LowLim)
 
+    return function
 
+    """
 def func_dictionary():
     dictionary = {}
 
@@ -67,18 +133,18 @@ def func_dictionary():
     dictionary["GaussJohnson"] = GaussJohnson()
 
     return dictionary
+"""
 
 
 if __name__ == "__main__":
-    f = DoubleGaussian()
-    a = ROOT.TF1("a", f, 0., 20, 6)
-    a.SetParameters(1, 0.3, 5, 2, 15, 3)
-
-    d = func_dictionary()
-
-    print(d.values())
 
     c = ROOT.TCanvas()
     c.cd()
+
+    a = BreitWigner(5, 5.6, pars=(1, 5.28, 0.02))
+
+    # d = func_dictionary()
+    # print(d.values())
+
     a.Draw()
     c.SaveAs("prova_func.png")
--- template_fit/template_fit.py
+++ template_fit/template_fit.py
@@ -6,53 +6,49 @@ population present in the dataset Bhh_data.root
 import time
 import ROOT
 import numpy as np
-from model_functions import func_dictionary
+from model_functions import BreitWigner, DoubleGaussian, GaussJohnson
 
 
-def fit_on_montecarlo(df, var, pars, fitfunc, fitfunc_name="fitfunc", Nbins=1000,
-                      h_name="h", h_title="h", LowLim=5, UpLim=5.6):
+def fit_on_montecarlo(df, var, fitfunc, img_name="template.png", Nbins=1000,
+                      h_name="h", h_title="h", LowLim=5.0, UpLim=5.6):
     """
     """
-    ROOT.gStyle.SetOptStat(0)
-    ROOT.gStyle.SetOptFit(1111)
+    ROOT.gStyle.SetOptFit(11111)
     h = df.Histo1D((h_name, h_title, Nbins, LowLim, UpLim), var)
 
-    [fitfunc.SetParameter(k, pars[k]) for k in range(len(pars))]
-    fitvar = h.Fit("fitfunc", "SQLR")
+    # [fitfunc.SetParameter(k, pars[k]) for k in range(len(pars))]
+    fitvar = h.Fit(fitfunc, "SQLR")
 
     c = ROOT.TCanvas()
     c.cd()
     h.Draw()
     fitfunc.Draw("same")
-    c.SaveAs("prova_template.png")
+    c.SaveAs(img_name)
 
     print(f'Chi2 = {fitvar.Chi2()}')
     print(f'Ndof = {fitvar.Ndf()}')
     print(f'Prob = {fitvar.Prob()}')
 
+    '''
     exit_pars = []
-    for k in range(len(pars)):
+    for k in range(fitfunc.GetNpar()):
         exit_pars.append(fitfunc.GetParameter(k))
+    '''
 
-    return exit_pars
+    return fitfunc, h
 
 
-def template_composition(model1, model2):
+def fit_on_data(df, var, templ_func_pi, templ_func_k, img_name="template.png",
+                Nbins=1000, h_name="h", h_title="h", LowLim=5., UpLim=5.6):
     """
     """
-
-
-def fit_on_data(df, var, par_pi, par_k, model_pi, model_k, Nbins=1000,
-                h_name="h", h_title="h", LowLim=5., UpLim=5.6):
-    """
-    """
-    ROOT.gStyle.SetOptStat(0)
-    ROOT.gStyle.SetOptFit(1111)
+    #ROOT.gStyle.SetOptStat(0)
+    ROOT.gStyle.SetOptFit(11111)
     h = df.Histo1D((h_name, h_title, Nbins, LowLim, UpLim), var)
 
-    f_pi = ROOT.TF1("f_pi", model_pi, LowLim, UpLim)
-    f_k = ROOT.TF1("f_k", model_k, LowLim, UpLim)
-    f_data = ROOT.TF1("f_data", "[0]( 1-[1])*f_pi + [1]*f_k )", LowLim, UpLim)
+    templ_func_pi.SetTitle("f_pi")
+    templ_func_k.SetTitle("f_k")
+    f_data = ROOT.TF1("f_data", "[0]*( 1-[1])*f_pi + [1]*f_k )", LowLim, UpLim)
 
 
 if __name__ == "__main__":
@@ -61,17 +57,16 @@ if __name__ == "__main__":
     tree = 't_M0pipi;2'
     var = 'M0_Mpipi'
 
-    functions = func_dictionary()
+    # functions = func_dictionary()
 
     df_pi, df_k = ROOT.RDataFrame(tree, file1), ROOT.RDataFrame(tree, file2)
 
-    pars = (8000, 0.5, 5.28, 0.015, 5.28, 0.015)
+    #pars = (8000, 0.5, 5.28, 0.015, 5.28, 0.015)
     #pars = (2290, 5.28, 0.016)
 
-    fitfunc = ROOT.TF1("fitfunc", functions["DoubleGaussian"], 5.02, 5.42)
-    fitfunc.SetParLimits(1, 0, 1)
-    fitfunc.SetParLimits(2, 5.2, 5.3)
-    fitfunc.SetParLimits(4, 5.2, 5.3)
-
-    exit_pars = fit_on_montecarlo(df_pi, var, pars, fitfunc)
-    print(exit_pars)
+    templ_pi = fit_on_montecarlo(df_pi, var, DoubleGaussian(5.02, 5.42),
+                                 img_name='template_fit_pi.png',
+                                 h_title='M_pipi distribution (B0->PiPi MC)')
+    templ_k = fit_on_montecarlo(df_k, var, GaussJohnson(5.02, 5.42),
+                                img_name='template_fit_k.png',
+                                h_title='M_pipi distribution (B0s->KK MC)')
