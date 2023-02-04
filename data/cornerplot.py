862a5796776cce989a0af9d25c6760613df66120
Lorenzo Punzi <punzilorenzo@gmail.com>
2023-02-04 17:08:43 +0100

    cornerplot changes

--- data/cornerplot.py
+++ data/cornerplot.py
@@ -8,13 +8,19 @@ from matplotlib import pyplot as plt
 
 def cornerplot(array):
 
-    return corner.corner(array[:,:-1])
+    array_new = array[:,:-1]
+    figure = corner.corner(array_new)
+    corner.overplot_lines(figure, np.mean(array_new,axis=0), color="C1")
+    corner.overplot_lines(figure, np.mean(array_new,axis=1), color="C2")
+    #corner.overplot_points(figure, value1[None], marker="s", color="C1")
+    #corner.overplot_points(figure, value2[None], marker="s", color="C2")
 
+    return figure
 
 #if __name__ == '___main__':
 arr_trainset = np.loadtxt('./txt/train_array_prova.txt')
 figure = cornerplot(arr_trainset)
-figure.set_size_inches(5, 5)
+figure.set_size_inches(9,7)
 figure.savefig('cornerplot.png')
 plt.show()
 
