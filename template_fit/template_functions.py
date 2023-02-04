b8f5502354eb77a8f0b125a73f3d5e21ee0446b8
Lorenzo Punzi <punzilorenzo@gmail.com>
2023-02-04 18:53:55 +0100

    last small commit of the bunch

---
+++ template_fit/template_functions.py
@@ -0,0 +1,130 @@
+"""
+"""
+
+import ROOT
+import numpy as np
+
+
+def InitializeFunctionsLibrary():
+    """
+    Function that loads into the Python environment the shared library
+    containing the fit functions.
+    """
+    ROOT.gInterpreter.ProcessLine('#include "header.h"')
+    ROOT.gSystem.Load(
+        '~/cmepda/cmepda-PiKclassifier/template_fit/func_library.so')
+
+
+def BreitWigner(LowLim, UpLim, funcname='BW', pars=(2000, 5.28, 0.5)):
+    """
+    Returns a TF1 function corresponding to a relativistic Breit-Wigner.
+    The parameters are written in C++ ROOT style and represent the following
+    quantities:
+        [0] = Normalization
+        [1] = Mass
+        [2] = Gamma
+
+    Parameters
+    ----------
+    LowLim: double
+        Lower limit of the x axis
+    UpLim: double
+        Upper limit of the x axis
+    funcname: string
+        Name of the function created
+    pars: tuple of double
+        Initial parameters given to the function
+    """
+
+    arg_gamma = 'TMath::Sqrt([1]*[1]*([2]*[2] + [1]*[1]))'
+    arg_kappa = '([1]*[2]*' + arg_gamma + \
+        '/TMath::Sqrt([1]*[1] + ' + arg_gamma + '))'
+    arg_denom_1 = '(x*x - [1]*[1])*(x*x - [1]*[1])'
+    str = '[0]*6e-4*0.9*' + arg_kappa + \
+        '/(' + arg_denom_1 + '+ [1]*[1]*[2]*[2])'
+
+    function = ROOT.TF1(funcname, str, LowLim, UpLim, 3)
+    print(type(function))
+
+    [function.SetParameter(k, pars[k]) for k in range(3)]
+    function.SetParLimits(0, 0., 1e10)
+    function.SetParLimits(1, LowLim, UpLim)
+    function.SetParLimits(2, 0., UpLim-LowLim)
+
+    return function
+
+
+def DoubleGaussian(LowLim, UpLim, funcname='DoubleGaussian',
+                   pars=(1.4e5, 0.5, 5.28, 0.02, 5.28, 0.02)):
+    """
+    Returns a TF1 function corresponding to a weighted sum of two Gaussians;
+    its expression is written in a C++ shared library and is imported in the
+    Python environment by the InitializeFunctionsLibrary() function.
+
+    Parameters
+    ----------
+    LowLim: double
+        Lower limit of the x axis
+    UpLim: double
+        Upper limit of the x axis
+    funcname: string
+        Name of the function created
+    pars: tuple of double
+        Initial parameters given to the function
+    """
+    InitializeFunctionsLibrary()
+
+    function = ROOT.TF1(funcname, ROOT.DoubleGaussian, LowLim, UpLim, 6)
+
+    [function.SetParameter(k, pars[k]) for k in range(6)]
+
+    function.SetParLimits(1, 0, 1)
+    function.SetParLimits(2, LowLim, UpLim)
+    function.SetParLimits(3, 0, UpLim-LowLim)
+    function.SetParLimits(4, LowLim, UpLim)
+    function.SetParLimits(5, 0, UpLim-LowLim)
+
+    return function
+
+
+def GaussJohnson(LowLim, UpLim, funcname='Gauss+Johnson',
+                 pars=(1.4e05, 0.99, 1.75, 0.043, 5.29, 0.85, 5.19, 0.06)):
+    """
+    Returns a TF1 function corresponding to a weighted sum of a Johnson and a
+    Gaussian; its expression is written in a C++ shared library and is imported
+    in the Python environment by the InitializeFunctionsLibrary() function.
+
+    Parameters
+    ----------
+    LowLim: double
+        Lower limit of the x axis
+    UpLim: double
+        Upper limit of the x axis
+    funcname: string
+        Name of the function created
+    pars: tuple of double
+        Initial parameters given to the function
+    """
+    InitializeFunctionsLibrary()
+
+    function = ROOT.TF1(funcname, ROOT.GaussJohnson, LowLim, UpLim, 8)
+    [function.SetParameter(k, pars[k]) for k in range(8)]
+
+    function.SetParLimits(1, 0, 1)
+    [function.SetParLimits(k, 0., 10.) for k in range(2, 6)]
+    function.SetParLimits(6, LowLim, UpLim)
+    function.SetParLimits(7, 0, UpLim-LowLim)
+
+    return function
+
+
+if __name__ == "__main__":
+
+    c = ROOT.TCanvas()
+    c.cd()
+
+    a = GaussJohnson(5., 5.6, pars=(
+                     1.4e05, 0.99, 1.75, 0.043, 5.29, 0.85, 5.19, 0.06))
+
+    a.Draw()
+    c.SaveAs("prova_func.png")
