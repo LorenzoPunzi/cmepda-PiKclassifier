97940e7dd1b3941a3d6dee13cbe21b5ea5146695
rubenforti <rubenforti27@gmail.com>
2023-02-14 18:37:52 +0100

    merge_variables optimized with KS output saved

--- data/merge_variables.py
+++ data/merge_variables.py
@@ -5,6 +5,7 @@ distributions for the the two species
 """
 import uproot
 import os
+import time
 import ROOT
 import numpy as np
 import matplotlib.pyplot as plt
@@ -42,9 +43,6 @@ def mergevar(tree_pi, tree_k, *vars):
     [v1_pi, v2_pi] = [tree_pi[var].array(library='np') for var in vars]
     [v1_K, v2_K] = [tree_k[var].array(library='np') for var in vars]
 
-    KS_stat_1 = KS(v1_pi, v1_K, alternative='twosided').statistic
-    KS_stat_2 = KS(v2_pi, v2_K, alternative='twosided').statistic
-
     v1lim = [min(min(v1_pi), min(v1_K)), max(max(v1_pi), max(v1_K))]
     v2lim = [min(min(v2_pi), min(v2_K)), max(max(v2_pi), max(v2_K))]
 
@@ -59,11 +57,13 @@ def mergevar(tree_pi, tree_k, *vars):
     kolmogorov_stat, m = KS_optimization(
         v_pi, v_K, parlims=[-100., 100.], n_try=1000)
 
+    KS_stats = []
+    KS_stats.append(kolmogorov_stat)
+    KS_stats.append(KS(v1_pi, v1_K, alternative='twosided').statistic)
+    KS_stats.append(KS(v2_pi, v2_K, alternative='twosided').statistic)
+
     v_merge_pi = mix_function(v1_pi, v2_pi, m)
     v_merge_K = mix_function(v1_K, v2_K, m)
-    print(f'Statistica di KS per la nuova variabile = {kolmogorov_stat}')
-    print(f'Stat. di KS per variabili originali: {KS_stat_1}, {KS_stat_2}')
-    print(' ')
 
     plt.figure(1)
     plt.subplot(2, 1, 1)
@@ -78,10 +78,11 @@ def mergevar(tree_pi, tree_k, *vars):
     plt.savefig('fig/'+vars[0]+'_'+vars[1]+'_merged_'+str(m)+'.pdf')
     plt.close()
 
-    return v_merge_pi, v_merge_K, m
+    return v_merge_pi, v_merge_K, m, KS_stats
 
 
 if __name__ == '__main__':
+    t0 = time.time()
     current_path = os.path.dirname(__file__)
     tree = 't_M0pipi;2'
     filepath = '../root_files'
@@ -92,26 +93,40 @@ if __name__ == '__main__':
 
     vars = ['M0_MKK', 'M0_MKpi', 'M0_MpiK', 'M0_Mpipi', 'M0_p', 'M0_pt']
     # Si può sicuramente fare in modo più figo con un parser
-    comb1 = np.array([0, 4])
-    comb2 = np.array([3, 4])
-    comb3 = np.array([1, 2])
-    # comb4 = np.array([])
-    combinations = [comb1, comb2, comb3]
 
-    # merged_pi = []
-    # merged_K = []
+    combinations = []
+    combinations.append(np.array([1, 2]))  # MKpi - MpiK
+    combinations.append(np.array([0, 4]))  # MKK - p
+    combinations.append(np.array([3, 4]))  # Mpipi - p
+    combinations.append(np.array([4, 3]))  # p - Mpipi
+    combinations.append(np.array([4, 0]))  # p - MKK
+    combinations.append(np.array([2, 4]))  # MpiK - p
+    combinations.append(np.array([1, 4]))  # MKpi - p
+    combinations.append(np.array([0, 1]))  # MKK - MKpi
+    combinations.append(np.array([0, 2]))  # MKK - MpiK
+
+    # comb4 = np.array([])
+    stats = []
+    str_combinations = []
 
     for comb in combinations:
         selected_vars = [vars[comb[0]], vars[comb[1]]]
         print(selected_vars)
-        v_merge_pi, v_merge_K, m = mergevar(tree_pi, tree_k, *selected_vars)
-        # merged_pi.append(v_merge_pi)
-        # merged_K.append(v_merge_K)
+        v_merge_pi, v_merge_K, m, stats_new = mergevar(
+            tree_pi, tree_k, *selected_vars)
+        stats.append(stats_new)
         print(m)
-        np.savetxt('txt/newvars_pi/'+vars[comb[0]]+'_'
-                   + vars[comb[1]]+'_merged_'+str(m)+'.txt', v_merge_pi)
-        np.savetxt('txt/newvars_k/'+vars[comb[0]]+'_'
-                   + vars[comb[1]]+'_merged_'+str(m)+'.txt', v_merge_K)
+        string_combination = vars[comb[0]]+'_'+vars[comb[1]]+'_merged_'
+        np.savetxt('txt/newvars_pi/'+string_combination
+                   + str(m)+'.txt', v_merge_pi)
+        np.savetxt('txt/newvars_k/'+string_combination
+                   + str(m)+'.txt', v_merge_K)
+        str_combinations.append(string_combination)
 
-    # newvars_pi = np.stack(merged_pi, axis=1)
-    # newvars_pi = np.stack(merged_pi, axis=1)
+    arr_stats = np.array(stats).reshape(len(combinations), 3)
+    np.savetxt('txt/output_KS_merged.txt', arr_stats,
+               delimiter='    ', header=''.join(str_combinations))
+
+    t1 = time.time()
+
+    print(f"Tempo totale = {t1-t0}")
