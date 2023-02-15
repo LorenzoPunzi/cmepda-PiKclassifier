2b48968d900e36f903423dd60f158221acde6688
rubenforti <rubenforti27@gmail.com>
2023-02-15 18:42:30 +0100

    ML training divided into functions

--- machine_learning/training.py
+++ machine_learning/training.py
@@ -2,29 +2,27 @@
 Trains a DNN with a numpy array with variable data columns to distinguish between pions and Kaons given multiple variables (features) on which to train simultaneously
 """
 
+import time
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.layers import Dense, Input
 from keras.models import Model
-import time
 
-def deepneuralnetwork(training_array, data_array, neurhid1 = 20, neurhid2 = 10, neurhid3 = 5 , epochnum = 100, plotflag = False, verb = 0, testdata = True):
+
+def train_DNN(training_array, neurhid1=20, neurhid2=10, neurhid3=5, epochnum=100, plotflag=False, verb=0, testdata=True):
     """
     """
     np.random.seed(int(time.time()))
 
     training_set = np.loadtxt(training_array)
- 
-    #print(training_set)
+    pid = training_set[:, -1]
+    features = training_set[:, :-1]
+    # print(pid)
+    # print(np.max(pid))
+    # print(features)
 
-    pid = training_set[:,-1]
-    features = training_set[:,:-1] 
-
-    #print(pid)
-    #print(np.max(pid))
-    #print(features)
-
-    inputlayer=Input(shape=(np.shape(features)[1],))
+    inputlayer = Input(shape=(np.shape(features)[1],))
     hiddenlayer = Dense(neurhid1, activation='relu')(inputlayer)
     hiddenlayer = Dense(neurhid2, activation='relu')(hiddenlayer)
     hiddenlayer = Dense(neurhid3, activation='relu')(hiddenlayer)
@@ -33,7 +31,8 @@ def deepneuralnetwork(training_array, data_array, neurhid1 = 20, neurhid2 = 10, 
     deepnn.compile(loss='binary_crossentropy', optimizer='adam')
     deepnn.summary()
 
-    history = deepnn.fit(features, pid, validation_split=0.5, epochs=epochnum, verbose=verb, batch_size=256)
+    history = deepnn.fit(features, pid, validation_split=0.5,
+                         epochs=epochnum, verbose=verb, batch_size=256)
 
     plt.figure('Losses')
     plt.xlabel('Epoch')
@@ -42,18 +41,31 @@ def deepneuralnetwork(training_array, data_array, neurhid1 = 20, neurhid2 = 10, 
     plt.plot(history.history['val_loss'], label='Validation Loss')
     plt.plot(history.history['loss'], label='Training Loss')
     plt.legend()
+    if plotflag:
+        plt.savefig(os.path.join('fig', "epochs"))
 
-    data_set = np.loadtxt(data_array)[:,:-1] if testdata else np.loadtxt(data_array)
+    return deepnn
+
+
+if __name__ == '__main__':
+    current_path = os.path.dirname(__file__)
+    txt_path = '../data/txt'
+    train_array_path = os.path.join(
+        current_path, txt_path, 'train_array_prova.txt')
+    data_array_path = os.path.join(
+        current_path, txt_path, 'data_array_prova.txt')
+
+    deepnn = train_DNN(train_array_path, plotflag=True, verb=0)
+
+    testdata = True
+    data_set = np.loadtxt(data_array_path)[
+                          :, :-1] if testdata else np.loadtxt(data_array_path)
 
     pred_array = deepnn.predict(data_set)
     print(data_set)
     f_pred = np.sum(pred_array)
     print(f'The predicted K fraction is : {f_pred/len(pred_array)}')
-    print('Max prediction :',np.max(pred_array))
-    print('Min prediction :',np.min(pred_array))
-    if plotflag : plt.show()
+    print('Max prediction :', np.max(pred_array))
+    print('Min prediction :', np.min(pred_array))
 
-    return pred_array
-
-if __name__ == '__main__':
-    predicted = deepneuralnetwork('./data/txt/train_array_prova.txt','./data/txt/data_array_prova.txt', plotflag = True, verb = 0)
+    # return pred_array
