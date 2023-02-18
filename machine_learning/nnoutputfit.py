30df1778b62358813c86878c81d058f970265b9a
Lorenzo Punzi <punzilorenzo@gmail.com>
2023-02-18 12:00:02 +0100

    added roc and various changes to nnoutputfit.py and training.py

--- machine_learning/nnoutputfit.py
+++ machine_learning/nnoutputfit.py
@@ -6,20 +6,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from training import dnn
 
-def find_cut(pi_array, k_array, efficiency, efficiency_mode = True):
+def find_cut(pi_array, k_array, efficiency, inverse_mode = False):
 
-    y = 0
-    if efficiency_mode: #Could test what algorithm is fastest
-        y = -np.sort(-k_array)[int(efficiency*len(k_array))] # piu o meno 1?
-    else:
-        y = np.sort(pi_array)[int(efficiency*len(pi_array))]
+    y = -np.sort(-k_array)[int(efficiency*len(k_array))] if not inverse_mode else np.sort(pi_array)[int(efficiency*len(pi_array))]
+    misid = (pi_array>y).sum(axis=0)/pi_array.size if not inverse_mode else (k_array>y).sum(axis=0)/k_array.size
     
-    return y
+    return y , misid
+
+def roc(pi_array, k_array, eff_span = (0.5,0.95,50)):
+    rocpnts = [((pi_array>y).sum(axis=0)/pi_array.size,(k_array>y).sum(axis=0)/k_array.size) for y in np.linspace(*eff_span)]
+    rocx, rocy = zip(*rocpnts)
+    plt.figure('roc')
+    plt.plot(rocx,rocy)
+    plt.xlabel('False Positive Probability')
+    plt.ylabel('True Positive Probability')
+    plt.draw()
+    plt.savefig('./fig/roc.pdf')
 
 if __name__ == '__main__':
 
     pi_eval, k_eval, data_eval = dnn('train_array_prova.txt','data_array_prova.txt',flagged_data=True)
-    y_cut = find_cut(pi_eval, k_eval, 0.9)
-    plt.axvline(x = y_cut, color = 'green')
-    plt.show() 
+    efficiency = 0.9
 
+    y_cut , misid = find_cut(pi_eval, k_eval, efficiency)
+    plt.axvline(x = y_cut, color = 'green', label = 'y cut for '+str(efficiency)+' efficiency')
+    plt.legend()
+    plt.savefig('./fig/ycut.pdf')
+    roc(pi_eval,k_eval)
+    
+    print(f'y cut is {y_cut} , misid is {misid}') #Why is it an ARRAY??? I can't understand
+    f = ((data_eval>y_cut).sum(axis=0)/data_eval.size-misid)/(efficiency-misid)
+    print(f'The estimated fraction of K events is {f}') 
+
+    plt.show()
--- machine_learning/training.py
+++ machine_learning/training.py
@@ -10,7 +10,7 @@ from keras.layers import Dense, Input
 from keras.models import Model
 
 
-def train_dnn(training_set, neurons = [20,10,5], epochnum=100, plotflag=False, verb=0):
+def train_dnn(training_set, neurons = [20,10,5], epochnum=100, showhistory=False, verb=0):
     """
     """
     np.random.seed(int(time.time()))
@@ -33,7 +33,7 @@ def train_dnn(training_set, neurons = [20,10,5], epochnum=100, plotflag=False, v
     history = deepnn.fit(features, pid, validation_split=0.5,
                          epochs=epochnum, verbose=verb, batch_size=128)
 
-    if plotflag:
+    if showhistory:
         plt.figure('Losses')
         plt.xlabel('Epoch')
         plt.ylabel('Binary CrossEntropy Loss')
@@ -47,15 +47,18 @@ def train_dnn(training_set, neurons = [20,10,5], epochnum=100, plotflag=False, v
     return deepnn
 
 
-def eval_dnn(dnn, eval_set, plot= [], test_data = False, f_print = False):
+def eval_dnn(dnn, eval_set, plotoptions= [], test_data = False, f_print = False):
     """
     """
     prediction_array = dnn.predict(eval_set) if not test_data else dnn.predict(eval_set[:,:-1])
     
-    if plot:
-        plotname = plot[0]
+    if plotoptions:
+        plotname = plotoptions[0]
         plt.figure(plotname)
-        plt.hist(prediction_array, bins=300, histtype='step', color=plot[1])
+        plt.hist(prediction_array, bins=300, histtype='step', color=plotoptions[1], label= plotoptions[2])
+        plt.xlabel('y')
+        plt.ylabel('Events per 1/300') #MAKE IT BETTER
+        plt.legend()
         plt.savefig('./fig/predict_'+plotname+'.pdf')
         plt.draw()
     
@@ -67,7 +70,7 @@ def eval_dnn(dnn, eval_set, plot= [], test_data = False, f_print = False):
 
     return prediction_array
 
-def dnn(trainarrayname, dataarrayname, txt_path = '../data/txt', flagged_data = False, plot_pi = ['Templ_eval', 'red'], plot_K = ['Templ_eval', 'blue'], plot_data = ['Dataeval','blue']):
+def dnn(trainarrayname, dataarrayname, txt_path = '../data/txt', flagged_data = False, plotoptions_pi = ['Templ_eval', 'red', 'Evalueated pions'], plotoptions_K = ['Templ_eval', 'blue', 'Evaluated kaons'], plotoptions_data = ['Dataeval','blue', 'Evaluated data']):
     
     current_path = os.path.dirname(__file__)
     train_array_path = os.path.join(
@@ -78,12 +81,14 @@ def dnn(trainarrayname, dataarrayname, txt_path = '../data/txt', flagged_data = 
     training_set = np.loadtxt(train_array_path)
     pi_set = np.array([training_set[i,:] for i in range(np.shape(training_set)[0]) if training_set[i,-1]== 0])
     K_set = np.array([training_set[i,:] for i in range(np.shape(training_set)[0]) if training_set[i,-1]== 1])
+    print(pi_set[:3,:])
     data_set = np.loadtxt(data_array_path)
 
-    deepnn = train_dnn(training_set,neurons=[20,20,20], verb=0) 
-    pred_array = eval_dnn(deepnn,data_set,plot=plot_data,test_data=flagged_data,f_print=True)
-    pi_eval = eval_dnn(deepnn, pi_set,plot=plot_pi,test_data=True)
-    K_eval = eval_dnn(deepnn, K_set,plot=plot_K,test_data=True)
+    deepnn = train_dnn(training_set,neurons=[20,20,15,10], verb=0, showhistory=True) 
+    pred_array = eval_dnn(deepnn,data_set,plotoptions=plotoptions_data,test_data=flagged_data,f_print=True)
+    pi_eval = eval_dnn(deepnn, pi_set,plotoptions=plotoptions_pi,test_data=True)
+    K_eval = eval_dnn(deepnn, K_set,plotoptions=plotoptions_K,test_data=True)
+    print(pi_eval)
 
     return pi_eval, K_eval, pred_array
 
