38cc99fd75cfbb716857663416d26228cf4c9a7c
Lorenzo Punzi <punzilorenzo@gmail.com>
2023-02-20 20:13:28 +0100

    test_nnoutputfit.py

---
+++ tests/test_nnoutputfit.py
@@ -0,0 +1,16 @@
+"""
+Tests some functions inside nnoutputfit.py
+"""
+import unittest
+import numpy as np
+import matplotlib.pyplot as plt
+
+class TestFindCut(unittest.TestCase):
+    def test_linspace(self):
+        """
+        Test with a linearly spaced numpy array
+        """
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
