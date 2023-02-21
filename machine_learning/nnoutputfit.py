734065bfca8d997d1f0575c08be1b5edae94a3ec
Lorenzo Punzi <punzilorenzo@gmail.com>
2023-02-21 17:03:42 +0100

    test_nnout modified and removed axis=0 from the .sum() in nnout.py

--- machine_learning/nnoutputfit.py
+++ machine_learning/nnoutputfit.py
@@ -1,20 +1,20 @@
 """
-Analyses the output array of deepneuralnetwork
+Analyses the output dnn() function in training.py
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-from training import dnn
+#from training import dnn
 
 def find_cut(pi_array, k_array, efficiency, inverse_mode = False):
 
     y = -np.sort(-k_array)[int(efficiency*len(k_array))] if not inverse_mode else np.sort(pi_array)[int(efficiency*len(pi_array))]
-    misid = (pi_array>y).sum(axis=0)/pi_array.size if not inverse_mode else (k_array>y).sum(axis=0)/k_array.size
-    
+    misid = (pi_array>y).sum()/pi_array.size if not inverse_mode else (k_array>y).sum()/k_array.size
+    #.sum() sums how many True there are in the masked (xx_array>y)
     return y , misid
 
 def roc(pi_array, k_array, eff_span = (0.5,0.95,50)):
-    rocpnts = [((pi_array>y).sum(axis=0)/pi_array.size,(k_array>y).sum(axis=0)/k_array.size) for y in np.linspace(*eff_span)]
+    rocpnts = [((pi_array>y).sum()/pi_array.size,(k_array>y).sum()/k_array.size) for y in np.linspace(*eff_span)]
     rocx, rocy = zip(*rocpnts)
     plt.figure('roc')
     plt.plot(rocx,rocy)
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     roc(pi_eval,k_eval)
     
     print(f'y cut is {y_cut} , misid is {misid}')
-    f = ((data_eval>y_cut).sum(axis=0)/data_eval.size-misid)/(efficiency-misid)
+    f = ((data_eval>y_cut).sum()/data_eval.size-misid)/(efficiency-misid)
     print(f'The estimated fraction of K events is {f}') 
 
     plt.show()
--- tests/test_nnoutputfit.py
+++ tests/test_nnoutputfit.py
@@ -4,13 +4,23 @@ Tests some functions inside nnoutputfit.py
 import unittest
 import numpy as np
 import matplotlib.pyplot as plt
+from machine_learning.training import dnn
+from machine_learning.nnoutputfit import find_cut
+
 
 class TestFindCut(unittest.TestCase):
     def test_linspace(self):
         """
         Test with a linearly spaced numpy array
         """
-        pass
+        test_array = np.linspace(0,1,1000)
+        for eff in np.linspace(0,1,20):
+            y_cut , _ = find_cut(test_array,test_array,eff,inverse_mode= False)
+            self.assertAlmostEqual(y_cut,(1-eff)*len(test_array))
+
+            y_cut , _ = find_cut(test_array,test_array,eff,inverse_mode= True)
+            self.assertAlmostEqual(y_cut,eff*len(test_array))
+        
 
 if __name__ == '__main__':
     unittest.main()
