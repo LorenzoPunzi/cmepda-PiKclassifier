68080337fd61a35b1cd5ea2e29685e58c5f7776f
rubenforti <rubenforti27@gmail.com>
2023-02-26 20:40:38 +0100

    Test on template_fit

--- tests/test_template_fit.py
+++ tests/test_template_fit.py
@@ -1,43 +1,38 @@
 import unittest
+import sys
 import time
 import ROOT
 import numpy as np
-from template_fit.template_functions import DoubleGaussian, GaussJohnson
+from template_fit.template_fit import fit_templates
+# from templatefit.template_functions import DoubleGaussian, GaussJohnson
+
+
+file1 = '../root_files/tree_B0PiPi.root'
+file2 = '../root_files/tree_B0sKK.root'
+file_data = '../root_files/tree_Bhh_data.root'
+tree = 't_M0pipi;1'
+var = 'M0_Mpipi'
+
+df_pi, df_k = ROOT.RDataFrame(tree, file1), ROOT.RDataFrame(tree, file2)
+
 
 class TestTemplateFit(unittest.TestCase):
     def test_onlypions(self):
         """
-        Test find_cut() with a linearly spaced numpy array
         """
-        test_array = np.linspace(0,1,1000)
-        for eff in np.linspace(0,1,20):
-            y_cut , _ = find_cut(test_array,test_array,eff,inverse_mode= False)
-            self.assertAlmostEqual((test_array>y_cut).sum()/test_array.size,eff, delta=5e-3)
-    
+        print('Running only_pions test')
+        res = fit_templates([df_pi, df_k, df_pi], var, LowLim=5.02, UpLim=5.42)
+        estim_f = round(res.Parameters()[1], 2)
+        self.assertAlmostEqual(estim_f, 0)
+
     def test_onlykaons(self):
         """
-        Test find_cut() with a linearly spaced numpy array
         """
-        test_array = np.linspace(0,1,1000)
-        for eff in np.linspace(0,1,20):
-            y_cut , _ = find_cut(test_array,test_array,eff,inverse_mode= False)
-            self.assertAlmostEqual((test_array>y_cut).sum()/test_array.size,eff, delta=5e-3)
-  
-    file1 = '../root_files/tree_B0PiPi_mc.root'
-    file2 = '../root_files/tree_B0sKK_mc.root'
-    file_data = '../root_files/tree_Bhh_data.root'
-    tree = 't_M0pipi;2'
-    var = 'M0_Mpipi'
+        print('Running only_kaons test')
+        res = fit_templates([df_pi, df_k, df_k], var, LowLim=5.02, UpLim=5.42)
+        estim_f = round(res.Parameters()[1], 2)
+        self.assertAlmostEqual(estim_f, 1)
 
-    df_pi, df_k = ROOT.RDataFrame(tree, file1), ROOT.RDataFrame(tree, file2)
 
-    templ_pi = fit_on_montecarlo(df_pi, var, DoubleGaussian(5.02, 5.42),
-                                 img_name='fig/template_fit_pi.png',
-                                 h_title='M_pipi distribution (B0->PiPi MC)')
-    templ_k = fit_on_montecarlo(df_k, var, GaussJohnson(5.02, 5.42),
-                                img_name='fig/template_fit_k.png',
-                                h_title='M_pipi distribution (B0s->KK MC)')
-
-    df_data = ROOT.RDataFrame('t_M0pipi;1', file_data)
-
-    fit_on_data(df_data, var, templ_pi, templ_k, LowLim=5.02, UpLim=5.42)
+if __name__ == '__main__':
+    unittest.main()
