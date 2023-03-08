f02f8dfca04c5c6c022730fcdec8582c6b1ad32c
rubenforti <rubenforti27@gmail.com>
2023-03-09 00:09:03 +0100

    template_fit fixed and more options added for analysis in main.py

--- __main__.py
+++ __main__.py
@@ -18,15 +18,23 @@
 """
 """
 import argparse
+import time
+import matplotlib.pyplot as plt
+from machine_learning.deepnn import dnn
+from template_fit.template_fit_var import fit_mc_template, global_fit
+from template_fit.template_functions import DoubleGaussian, GaussJohnson
 from utilities.gen_from_toy import gen_from_toy
-from utilities.utils import default_rootpaths, default_vars
-from template_fit.template_fit import fit_templates
+from utilities.dnn_settings import dnn_settings
+from utilities.utils import default_rootpaths, default_txtpaths, default_vars,\
+                            find_cut, roc, plot_rocs
+
 # print(" ----------------------------------------------- ")
 # print("|  Welcome to the PiK Classifier package!       |")
 # print("|                                               |")
 # print("|  Authors: Lorenzo Punzi, Ruben Forti          |")
 # print("|  Release: 1.0  -  march 2023                  |")
 # print(" ----------------------------------------------- ")
+
 
 default_toyMC_path = ('data/root_files/toyMC_B0PiPi.root',
                       'data/root_files/toyMC_B0sKK.root')
@@ -35,20 +43,21 @@ parser = argparse.ArgumentParser(prog='PiK classifier',
                                  description='What the program does',
                                  epilog='Text at the bottom of help')
 
+parser.add_argument('-g', '--generate', action='store_true',
+                    help='Generates MC and mixed datasets from the toyMC')
+
 parser.add_argument('-rp', '--rootpaths', nargs=2, default=default_toyMC_path,
                     help='Path of the toyMC root files taken as input')
 
 parser.add_argument('-tr', '--tree', default='t_M0pipi;1',
                     help='Name of the tree of the toyMC root files where the variables are stored')
 
-parser.add_argument('-g', '--generate', action='store_true',
-                    help='Generates MC and mixed datasets from the toyMC')
-
-parser.add_argument('-rpo', '--rootpaths_out', nargs=3, default=default_rootpaths(),
+parser.add_argument('-rds', '--root_datasets', nargs=3, default=default_rootpaths(),
                     help='Path of the datasets generated from the toyMC')
 
-parser.add_argument('-fp', '--figpath',
-                    help='Path where figures are going to be saved')
+parser.add_argument('-f', '--fraction', type=float, default=0.42,
+                    help='Fraction of Kaons in the mixed dataset you want to generate')
+
 
 parser.add_argument('-v', '--variables', nargs='+', default=default_vars(),
                     help='Variables you want to treat')
@@ -56,54 +65,107 @@ parser.add_argument('-v', '--variables', nargs='+', default=default_vars(),
 parser.add_argument('-vfit', '--var_fit', default='M0_Mpipi',
                     help='Variable on which template fit is performed')
 
-parser.add_argument('-f', '--fraction', type=float, default=0.42,
-                    help='Fraction of Kaons in the mixed dataset you want to generate')
+parser.add_argument('-vcut', '--var_cut', default='M0_Mpipi',
+                    help='Variable on which cut evaluation is performed')
 
 parser.add_argument('-t', '--type', nargs='+', default='all',
                     choices=['tfit', 'dnn', 'dtc', 'vcut', 'all'],
                     help='Type of the analysis to be performed')
 
 parser.add_argument('-cr', '--cornerplot', action='store_true',
-                    help='Choice to generate and save the cornerplot')
+                    help='Choice to generate and save the cornerplot of the variables given in input')
+
+parser.add_argument('-fp', '--figpath',
+                    help='Path where figures are going to be saved')
 
 args = parser.parse_args()
 
 
-filepaths = args.rootpaths
+filepaths = args.root_datasets
 figpath = args.figpath
 tree = args.tree
 
-var_fit = args.var_fit
-
 if args.generate:
+    # Generates the datasets with the requested fraction of Kaons in
+    # the mixed sample. If the following two quantities are BOTH set to
+    # zero, the function generates the datasets with the maximum
+    # possible number of events
+    NUM_MC = 0
+    NUM_DATA = 0
     gen_from_toy(filepaths_in=tuple(args.rootpaths), tree=tree, f=args.fraction,
-                 num_mc=100000, num_data=30000, vars=tuple(args.variables))
+                 n_mc=NUM_MC, n_data=NUM_DATA, vars=tuple(args.variables))
 
 
+# Initialize the appropriate analysis-methods list, also removing duplicates
 if 'all' in args.type:
     analysis = ['all']
 else:
     analysis = []
-    # Remove duplicates from analysis methods list
-    [analysis.append(item) for item in args.type if item not in analysis]
+    a = [analysis.append(item) for item in args.type if item not in analysis]
+
 
 for opt in analysis:
 
-    if opt == "tfit" or opt == "all":
+    if opt in ["tfit", "all"]:
+        # ~~~~~~~~ Setup of the template fit - free to edit ~~~~~~~~~~~
+        Nbins_histo = 1000
+        histo_lims = (5.0, 5.6)  # Limits of the histograms
+        fit_range = (5.02, 5.42)  # Range where the functions are fitted
+        p0_pi = (1e5, 0.14, 5.28, 0.08, 5.29, 0.02)
+        p0_k = (1e5, 0.96, 1.7, 0.6, 5.29, 0.7, 5.18, 0.05)
+        figures = True
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        t0 = time.time()
         print("\nTemplate fit with ROOT - working...\n")
-        res = fit_templates(args.rootpaths_out, 'tree;1', var_fit)
+        var = args.var_fit
+
+        templ_pars_pi = fit_mc_template(
+            filepaths[0], 'tree;1', var, DoubleGaussian(fit_range, pars=p0_pi),
+            Nbins=Nbins_histo, histo_lims=histo_lims,
+            histo_title=f'{var} distribution (B0->PiPi MC)', savefig=figures,
+            img_name='fig/template_fit_pi.png')
+        templ_pars_k = fit_mc_template(
+            filepaths[1], 'tree;1', var, GaussJohnson(fit_range, pars=p0_k),
+            Nbins=Nbins_histo, histo_lims=histo_lims,
+            histo_title=f'{var} distribution (B0s->KK MC)', savefig=figures,
+            img_name='fig/template_fit_k.png')
+
+        res = global_fit(filepaths[2], 'tree;1', var, Nbins=Nbins_histo,
+                         pars_mc1=templ_pars_k, pars_mc2=templ_pars_pi,
+                         histo_lims=histo_lims, savefigs=figures)
         print(f'Frazione di K = {res.Parameters()[1]} +- {res.Errors()[1]}')
-        print("\nTemplate fit with ROOT - ended successfully! \n\n")
+        t1 = time.time()
+        print(
+            f'\nTemplate fit with ROOT - ended successfully in {t1-t0} s \n\n')
 
-    if opt == "dnn" or opt == "all":
+    if opt in ["dnn", "all"]:
+        # ~~~~~~~~ Setup of the DNN - free to edit ~~~~~~~~~~~~~~~~~~~~
+        settings = dnn_settings()
+        settings.layers = [75, 60, 45, 30, 20]
+        settings.batch_size = 128
+        settings.epochnum = 10
+        settings.verbose = 2
+        settings.batchnorm = False
+        settings.dropout = 0.005
+        settings.learning_rate = 5e-4
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        print("\nDeep neural network - working...\n")
+        pi_eval, k_eval, data_eval = dnn(settings=settings)
+        efficiency = 0.95
+
+        y_cut, misid = find_cut(pi_eval, k_eval, efficiency)
+        plt.axvline(x=y_cut, color='green', label='y cut for '
+                    + str(efficiency)+' efficiency')
+        plt.legend()
+        plt.savefig('fig/ycut.pdf')
+
+        rocdnnx, rocdnny, aucdnn = roc(pi_eval, k_eval, eff=efficiency,
+                                       inverse_mode=False, makefig=True,
+                                       name="dnn_roc")
+        print("\nDeep neural network - ended successfully! \n\n")
+
+    if opt in ["dtc", "all"]:
         print("\nTemplate fit with ROOT - working...\n")
 
-    if opt == "dtc" or opt == "all":
+    if opt in ["vcut", "all"]:
         print("\nTemplate fit with ROOT - working...\n")
-
-    if opt == "vcut" or opt == "all":
-        print("\nTemplate fit with ROOT - working...\n")
-
-
-print(type(args.type))
-print(args.fraction)
--- template_fit/template_functions.py
+++ template_fit/template_functions.py
@@ -3,10 +3,11 @@
 
 import ROOT
 import os
+import sys
 from utilities.exceptions import LoadHeadError
 
 
-def InitializeFunctionsLibrary():
+def initialize_library():
     """
     Function that loads into the Python environment the shared library
     containing the fit functions.
@@ -19,9 +20,9 @@ def InitializeFunctionsLibrary():
             raise LoadHeadError(header_path)
     except LoadHeadError as err:
         print(err)
-        exit()
+        sys.exit()
     success = ROOT.gSystem.CompileMacro('fit_functions.cpp', opt="ks")
-    if not success: #!!!! Make it better
+    if not success:  # !!!! Make it better
         lib_file = ''
         for root, dirnames, filenames in os.walk("."):
             for filename in filenames:
@@ -34,7 +35,7 @@ def InitializeFunctionsLibrary():
             ROOT.gSystem.Load(lib_file)
 
 
-def BreitWigner(LowLim, UpLim, funcname='BW', pars=(2000, 5.28, 0.5)):
+def BreitWigner(func_limits, funcname='BW', pars=(2000, 5.28, 0.5)):
     """
     Returns a TF1 function corresponding to a relativistic Breit-Wigner.
     The parameters are written in C++ ROOT style and represent the following
@@ -62,19 +63,19 @@ def BreitWigner(LowLim, UpLim, funcname='BW', pars=(2000, 5.28, 0.5)):
     str = '[0]*6e-4*0.9*' + arg_kappa + \
         '/(' + arg_denom_1 + '+ [1]*[1]*[2]*[2])'
 
-    function = ROOT.TF1(funcname, str, LowLim, UpLim, 3)
+    function = ROOT.TF1(funcname, str, func_limits[0], func_limits[1], 3)
     print(type(function))
 
     [function.SetParameter(k, pars[k]) for k in range(3)]
     function.SetParLimits(0, 0., 1e10)
-    function.SetParLimits(1, LowLim, UpLim)
-    function.SetParLimits(2, 0., UpLim-LowLim)
+    function.SetParLimits(1, func_limits[0], func_limits[1])
+    function.SetParLimits(2, 0., func_limits[0], func_limits[1])
 
     return function
 
 
-def DoubleGaussian(LowLim, UpLim, funcname='DoubleGaussian',
-                   pars=(1.4e5, 0.6, 5.28, 0.04, 5.29, 0.02)):
+def DoubleGaussian(func_limits, funcname='DoubleGaussian',
+                   pars=(1e5, 0.14, 5.28, 0.08, 5.29, 0.02)):
     """
     Returns a TF1 function corresponding to a weighted sum of two Gaussians;
     its expression is written in a C++ shared library and is imported in the
@@ -91,23 +92,24 @@ def DoubleGaussian(LowLim, UpLim, funcname='DoubleGaussian',
     pars: tuple of double
         Initial parameters given to the function
     """
-    InitializeFunctionsLibrary()
+    initialize_library()
 
-    function = ROOT.TF1(funcname, ROOT.DoubleGaussian, LowLim, UpLim, 6)
+    function = ROOT.TF1(funcname, ROOT.DoubleGaussian,
+                        func_limits[0], func_limits[1], 6)
 
     [function.SetParameter(k, pars[k]) for k in range(6)]
 
     function.SetParLimits(1, 0, 1)
-    function.SetParLimits(2, LowLim, UpLim)
-    function.SetParLimits(3, 0, UpLim-LowLim)
-    function.SetParLimits(4, LowLim, UpLim)
-    function.SetParLimits(5, 0, UpLim-LowLim)
+    function.SetParLimits(2, func_limits[0], func_limits[1])
+    function.SetParLimits(3, 0, func_limits[1]-func_limits[0])
+    function.SetParLimits(4, func_limits[0], func_limits[1])
+    function.SetParLimits(5, 0, func_limits[1]-func_limits[0])
 
     return function
 
 
-def GaussJohnson(LowLim, UpLim, funcname='Gauss+Johnson',
-                 pars=(1.4e05, 0.99, 1.75, 0.043, 5.29, 0.85, 5.19, 0.06)):
+def GaussJohnson(func_limits, funcname='Gauss+Johnson',
+                 pars=(1e05, 0.96, 1.7, 0.6, 5.29, 0.7, 5.18, 0.05)):
     """
     Returns a TF1 function corresponding to a weighted sum of a Johnson and a
     Gaussian; its expression is written in a C++ shared library and is imported
@@ -124,15 +126,16 @@ def GaussJohnson(LowLim, UpLim, funcname='Gauss+Johnson',
     pars: tuple of double
         Initial parameters given to the function
     """
-    InitializeFunctionsLibrary()
+    initialize_library()
 
-    function = ROOT.TF1(funcname, ROOT.GaussJohnson, LowLim, UpLim, 8)
+    function = ROOT.TF1(funcname, ROOT.GaussJohnson,
+                        func_limits[0], func_limits[1], 8)
+
     [function.SetParameter(k, pars[k]) for k in range(8)]
-
     function.SetParLimits(1, 0, 1)
     [function.SetParLimits(k, 0., 10.) for k in range(2, 6)]
-    function.SetParLimits(6, LowLim, UpLim)
-    function.SetParLimits(7, 0, UpLim-LowLim)
+    function.SetParLimits(6, func_limits[0], func_limits[1])
+    function.SetParLimits(7, 0, func_limits[1]-func_limits[0])
 
     return function
 
