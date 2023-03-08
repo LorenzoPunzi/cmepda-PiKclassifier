54a6db7b4ebe1400391e7c8e1fa8bc1c24e3ba56
Lorenzo Punzi <punzilorenzo@gmail.com>
2023-03-08 16:56:51 +0000

    working ont he statistical uncertainties

--- uncertainties/statistical/stat_dtc.py
+++ uncertainties/statistical/stat_dtc.py
@@ -9,12 +9,12 @@ if __name__ == '__main__':
     predicted_array, eff, misid = dt_classifier(('txt',default_txtpaths()), print_tree='')
 
     print(predicted_array.size)
-    subdata = np.split(predicted_array,20)
+    subdata = np.split(predicted_array,400)
 
     fractions = [data.sum()/len(data) for data in subdata]
 
     plt.figure('Fraction distribution for dtc')
-    plt.hist(fractions,bins=300, histtype='step')
+    plt.hist(fractions,bins=20, histtype='step')
     plt.savefig('fig/fractionsdtc.pdf')
     plt.show()
 
--- utilities/import_datasets.py
+++ utilities/import_datasets.py
@@ -185,11 +185,11 @@ if __name__ == '__main__':
     tree = 'tree;1'
 
     v_mc, v_data = array_generator(rootpaths, tree, vars=default_vars(),
-                                   n_mc=560000,
+                                   n_mc=560000, n_data=100000,
                                    new_variables=[])
 
-    #np.savetxt('txt/train_array.txt', v_mc)
-    #np.savetxt('txt/data_array.txt', v_data)
+    np.savetxt('../data/txt/train_array.txt', v_mc)
+    np.savetxt('../data/txt/data_array.txt', v_data)
 
     t2 = time.time()
 
