e2eceb73598d904dd4dc57424ea61808eb0f2a51
Lorenzo Punzi <punzilorenzo@gmail.com>
2023-03-11 20:52:16 +0000

    added test_var_cut.py skeleton

---
+++ tests/test_var_cut.py
@@ -0,0 +1,17 @@
+"""
+"""
+
+import unittest
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from var_cut.var_cut import var_cut
+
+
+class TestVarCut(unittest.TestCase):
+    def test_uniform(self):
+        """
+        Test var_cut() with a two uniformly distributed histograms
+        """
+        
+
