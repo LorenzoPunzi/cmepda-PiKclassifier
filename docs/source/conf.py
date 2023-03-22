e66b4fd90693febe96ac065b3034afac837ac227
rubenforti <rubenforti27@gmail.com>
2023-03-22 18:46:52 +0100

    docs config.py update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,10 @@ import sys
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 """
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+
 
 project = 'cmepda-PiKclassifier'
 copyright = '2023, Ruben Forti and Lorenzo Punzi'
@@ -19,7 +23,7 @@ author = 'Ruben Forti and Lorenzo Punzi'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -32,7 +36,6 @@ autodoc_default_options = {
     'undoc-members': True,
     'exclude-members': '__weakref__'
 }
-
 
 
 # -- Options for HTML output -------------------------------------------------
