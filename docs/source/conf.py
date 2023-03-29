f4d9e32337c88765effa7874cdef6820fce946ca
rubenforti <rubenforti27@gmail.com>
2023-03-29 22:32:06 +0200

    keras as mock-module for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 
-autodoc_mock_imports = ['ROOT']
+autodoc_mock_imports = ['ROOT', 'keras']
 
 project = 'cmepda-PiKclassifier'
 copyright = '2023, Ruben Forti and Lorenzo Punzi'
