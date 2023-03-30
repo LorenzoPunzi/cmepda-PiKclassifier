73aa8642256da6ff218bbfec9d6b55b64a7edfe1
rubenforti <rubenforti27@gmail.com>
2023-03-30 13:15:24 +0200

    var_cut review

--- var_cut/var_cut.py
+++ var_cut/var_cut.py
@@ -1,15 +1,13 @@
 """
-
 """
-import warnings
 import sys
+import warnings
 import numpy as np
 from matplotlib import pyplot as plt
 from utilities.utils import default_rootpaths, default_figpath, \
                             find_cut, stat_error, syst_error
 from utilities.import_datasets import loadvars
 from utilities.exceptions import IncorrectEfficiencyError
-
 
 warnings.formatwarning = lambda msg, *args, **kwargs: f'\n{msg}\n'
 
@@ -18,38 +16,36 @@ def var_cut(rootpaths=default_rootpaths(), tree='tree;1', cut_var='M0_Mpipi',
             eff=0.90, error_optimization=True, inverse_mode=False,
             specificity_mode=False, savefig=False, figpath=''):
     """
-    Estimates the fraction of kaons in the mixed sample by performing a cut on
-    a specified variable in the template dataset. The cut point is chosen so
-    that the probability that a Kaon event has a larger value than that
-    (smaller, in the inverse-mode) is equal to the efficiency ("eff") chosen
+    Estimates the signal fraction in the data set by performing a cut on a
+    specified variable in the MC sets.
 
-    :param rootpaths: Tuple containing three .root file paths, for the "background" sample (flag=0), the "signal" sample (flag=1) and the mixed one, in this order.
+    :param rootpaths: Tuple containing three .root file paths, for the "background" set (flag=0), the "signal" set (flag=1) and the data one, in this order.
     :type rootpaths: tuple[str]
-    :param tree: Name of the tree from which to load
+    :param tree: Name of the tree from which to load the events.
     :type tree: str
-    :param cut_var: Variable to load and test
+    :param cut_var: Variable to load and test.
     :type cut_var: str
-    :param eff: Sensitivity required from the test (specificity in specificity mode)
+    :param eff: Sensitivity required from the test (specificity in specificity mode).
     :type eff: float
-    :param error_optimization: Performs error optimization instead of using a fixed efficiency value
+    :param error_optimization: Performs error optimization instead of using a fixed efficiency value.
     :type error_optimization: bool
-    :param inverse_mode: Set to ``True`` if the "signal" events have lower values of the cut variable than the "background" ones
+    :param inverse_mode: Set to ``True`` if the "signal" events have lower values of the cut variable than the "background" ones.
     :type inverse_mode: bool
-    :param specificity_mode: Set to ``True`` if the efficiency given is the specificity
+    :param specificity_mode: Set to ``True`` if the efficiency given is the specificity.
     :type specificity_mode: bool
-    :param savefig: Draw the figure of the variable distribution for the two species and the cut
+    :param savefig: Draw the figure of the variable distribution for the two MC sets and the cut performed.
     :type savefig: bool
-    :param figpath: Path to where to save the figure
+    :param figpath: Path to where to save the figure.
     :type figpath: str
-    :return: Estimated fraction of Kaons (with uncertainties), parameters of the test algorithm and arrays containing the DNN evaluation of the testing array (divided for species)
+    :return: Estimated fraction of signal (with uncertainties), parameters of the cut algorithm and MC sets of the variable used.
     :rtype: tuple[float], tuple[float], tuple[numpy.array[float]]
 
     """
-    rootpath_pi, rootpath_k, rootpath_data = rootpaths
-    var_pi, var_k = loadvars(rootpath_pi, rootpath_k, tree,
-                             [cut_var], flag_column=False)
-    var_data, _ = loadvars(rootpath_data, rootpath_data, tree,
-                           [cut_var], flag_column=False)
+
+    var_pi, var_k = loadvars(
+        rootpaths[0], rootpaths[1], tree, [cut_var], flag_column=False)
+    var_data, _ = loadvars(
+        rootpaths[2], rootpaths[2], tree, [cut_var], flag_column=False)
 
     used_eff = 0
     df_opt = -99999
@@ -64,12 +60,17 @@ def var_cut(rootpaths=default_rootpaths(), tree='tree;1', cut_var='M0_Mpipi',
     if error_optimization is True:  # Enables FOM maximization
         efficiencies = np.linspace(0.25, 0.999, 200)
         for tmp_eff in efficiencies:
-            tmp_cut, tmp_misid = find_cut(var_pi, var_k, tmp_eff, inverse_mode=inverse_mode,
+            tmp_cut, tmp_misid = find_cut(var_pi, var_k, tmp_eff,
+                                          inverse_mode=inverse_mode,
                                           specificity_mode=False)
-            tmp_frac = ((var_data < tmp_cut).sum()/var_data.size - tmp_misid)/(tmp_eff - tmp_misid) \
+
+            tmp_frac = ((var_data < tmp_cut).sum()/var_data.size-tmp_misid)/(tmp_eff - tmp_misid) \
                 if inverse_mode else ((var_data > tmp_cut).sum()/var_data.size-tmp_misid)/(tmp_eff - tmp_misid)
-            tmp_dfopt = -np.sqrt(stat_error(tmp_frac, var_data.size, tmp_eff, tmp_misid)
-                                 ** 2+syst_error(tmp_frac, (var_pi.size, var_k.size), tmp_eff, tmp_misid)**2)
+
+            tmp_dfopt = -np.sqrt(
+                stat_error(tmp_frac, var_data.size, tmp_eff, tmp_misid)**2
+                + syst_error(tmp_frac, (var_pi.size, var_k.size), tmp_eff, tmp_misid)**2)
+
             if tmp_dfopt >= df_opt:
                 df_opt = tmp_dfopt
                 used_eff = tmp_eff
@@ -80,8 +81,10 @@ def var_cut(rootpaths=default_rootpaths(), tree='tree;1', cut_var='M0_Mpipi',
                           specificity_mode=specificity_mode)
 
     if (specificity_mode is not True and misid > used_eff) or \
-            (specificity_mode is True and 1 - used_eff > misid):
-        msg = f'***WARNING*** \ninverse mode was called as {inverse_mode} in var_cut, but the test is not unbiased this way, switching to inverse_mode = {not inverse_mode}'
+       (specificity_mode is True and 1 - used_eff > misid):
+        msg = f'***WARNING***\nInverse mode was called as {inverse_mode} in \
+var_cut, but the test is not unbiased this way; switching to \
+inverse_mode = {not inverse_mode} \n*************\n'
         warnings.warn(msg, stacklevel=2)
         inverse_mode = not inverse_mode
         cut, misid = find_cut(var_pi, var_k, used_eff, inverse_mode=inverse_mode,
@@ -92,9 +95,9 @@ def var_cut(rootpaths=default_rootpaths(), tree='tree;1', cut_var='M0_Mpipi',
         range = (min(min(var_pi), min(var_k)), max(max(var_pi), max(var_k)))
         plt.figure('Cut on ' + cut_var)
         plt.hist(var_pi, nbins, range=range, histtype='step',
-                 color='red', label=cut_var + ' for Pions')
+                 color='red', label=f'{cut_var} for Pions')
         plt.hist(var_k, nbins, range=range, histtype='step',
-                 color='blue', label=cut_var + ' for Kaons')
+                 color='blue', label=f'{cut_var} for Kaons')
         plt.axvline(x=cut, color='green',
                     label=f'{cut_var} Cut for {used_eff} efficiency')
         plt.title(f'Varibale Cut on {cut_var}')
@@ -103,8 +106,8 @@ def var_cut(rootpaths=default_rootpaths(), tree='tree;1', cut_var='M0_Mpipi',
             f'Events per {round((range[1]-range[0])/nbins, 4)} [{cut_var}]')
         plt.legend()
         plt.draw()
-        plt.savefig(default_figpath('cut_'+cut_var)) \
-            if figpath == '' else plt.savefig(figpath+'/cut_'+cut_var+'.png')
+        plt.savefig(default_figpath(f'cut_{cut_var}')) \
+            if figpath == '' else plt.savefig(f'{figpath}/cut_{cut_var}.png')
 
     fraction = ((var_data < cut).sum()/var_data.size - misid)/(used_eff - misid) \
         if inverse_mode else ((var_data > cut).sum()/var_data.size-misid)/(used_eff - misid)
@@ -120,17 +123,17 @@ def var_cut(rootpaths=default_rootpaths(), tree='tree;1', cut_var='M0_Mpipi',
 
     fr = (fraction, df_stat, df_syst)
 
-    print(f'{cut_var} cut is {cut} for {used_eff} efficiency\n')
+    print(f'{cut_var} cut is {cut} for {used_eff} efficiency')
     print(
-        f'Misid is {misid} +- {np.sqrt(misid*(1-misid)/var_pi.size)} for {used_eff} efficiency\n')
+        f'Misid is {misid} +- {np.sqrt(misid*(1-misid)/var_pi.size)} for {used_eff} efficiency')
     print(
-        f'The estimated fraction of K events is {fraction} +- {df_stat} (stat) +- {df_syst} (syst)\n')
+        f'The estimated fraction of K events is {fraction} +- {df_stat} (stat) +- {df_syst} (syst)')
 
-    algorithm_parameters = (used_eff, misid, cut)
+    cut_parameters = (used_eff, misid, cut)
 
     eval_arrays = (var_pi, var_k)
 
-    return fr, algorithm_parameters, eval_arrays
+    return fr, cut_parameters, eval_arrays
 
 
 if __name__ == '__main__':
